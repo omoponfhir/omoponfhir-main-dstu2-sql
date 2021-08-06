948d634f4359464f90eb37d8080978d66becdbcf
Myung Choi <myungchoi@gmail.com>
2021-08-06 14:59:21 -0400

    naming pattern fix

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,1 +1,3 @@
-git submodule foreach git pull origin master
+git submodule update --remote -- omoponfhir-omopv5-sql
+git submodule update --remote -- omoponfhir-omopv5-dstu2-mapping
+git submodule update --remote -- omoponfhir-dstu2-server
